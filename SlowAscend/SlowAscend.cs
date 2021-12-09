e3159bf62344fe6d6d85b2c668d986671e81ddfb
Mattias Viklund <40642947+Mattias-Viklund@users.noreply.github.com>
2021-12-09 03:27:55 +0100

    Initial commit
    Yep

---
+++ SlowAscend/SlowAscend.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using QModManager.API.ModLoading;
+using HarmonyLib;
+using Subnautica;
+using UnityEngine;
+using Logger = QModManager.Utility.Logger;
+using SMLHelper.V2.Handlers;
+
+namespace SlowAscend
+{
+    [QModCore]
+    public static class SlowAscend
+    {
+        public static SlowAscendConfig Config { get; private set; }
+
+        // Your patching method must have the QModPatch attribute (and must be public)
+        [QModPatch]
+        public static void MyInitializationMethod()
+        {
+            Config = OptionsPanelHandler.Main.RegisterModOptions<SlowAscendConfig>();
+
+            // Add your patching code here
+            Harmony harmony = new Harmony("SlowAscent");
+            harmony.PatchAll();
+
+            Logger.Log(Logger.Level.Info, "Patched successfully!");
+
+        }
+    }
+
+    // Your patching method must have the QModPatch attribute (and must be public)
+    [HarmonyPatch(typeof(GameInput))]
+    [HarmonyPatch("GetMoveDirection")]
+    internal class GameInputPatcher
+    {
+        [HarmonyPostfix]
+        public static Vector3 GameInputGetMoveDirection(Vector3 param)
+        {
+            if (param.y > 0)
+            {
+                param.y *= SlowAscend.Config.ascentMultiplier;
+            }
+            return param;
+
+        }
+    }
+}
---
+++ SlowAscend/SlowAscendConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SMLHelper.V2.Json;
+using SMLHelper.V2.Utility;
+using SMLHelper.V2.Options;
+using SMLHelper.V2.Options.Attributes;
+
+namespace SlowAscend
+{
+    [Menu("Slow Ascent")]
+    public class SlowAscendConfig : ConfigFile
+    {
+        [Slider("Ascent Multiplier", 0.1f, 1.0f, DefaultValue = 0.35f)]
+        public float ascentMultiplier = 0.35f;
+
+    }
+}
